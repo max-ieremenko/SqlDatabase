76174b3c8ede0c18f487302ee2d937a3a38bc422
Max <max.ieremenko@gmail.com>
2018-03-30 11:33:52 +0200

    update readme

--- Sources/SqlDatabase/Properties/AssemblyInfo.cs
+++ Sources/SqlDatabase/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SqlDatabase")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2018 Maksym Ieremenko")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
