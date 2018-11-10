33291de23a57712807bb93fba8d654a7e8a7fb7b
Max Ieremenko <max.ieremenko@gmail.com>
2018-11-10 10:34:52 +0100

    - execution of assembly script in temporary directory - allow to create sub-domain

---
+++ Sources/SqlDatabase.Test/Scripts/AssemblyScriptTest.StepWithSubDomain.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using SqlDatabase.TestApi;
+
+namespace SqlDatabase.Scripts
+{
+    public partial class AssemblyScriptTest
+    {
+        public sealed class StepWithSubDomain
+        {
+            public void ShowAppBase(IDbCommand command)
+            {
+                var assembly = GetType().Assembly;
+
+                command.CommandText = assembly.Location;
+                command.ExecuteNonQuery();
+
+                command.CommandText = AppDomain.CurrentDomain.BaseDirectory;
+                command.ExecuteNonQuery();
+            }
+
+            public void ShowConfiguration(IDbCommand command)
+            {
+                command.CommandText = AppDomain.CurrentDomain.SetupInformation.ConfigurationFile;
+                command.ExecuteNonQuery();
+
+                command.CommandText = Query.ConnectionString;
+                command.ExecuteNonQuery();
+            }
+
+            public void Execute(IDbCommand command)
+            {
+                var assembly = GetType().Assembly;
+
+                var setup = new AppDomainSetup
+                {
+                    ApplicationBase = Path.GetDirectoryName(assembly.Location),
+                    ConfigurationFile = AppDomain.CurrentDomain.SetupInformation.ConfigurationFile
+                };
+
+                var domain = AppDomain.CreateDomain("StepWithSubDomain", null, setup);
+                try
+                {
+                    var agent = (StepDomainAgent)domain.CreateInstanceFromAndUnwrap(assembly.Location, typeof(StepDomainAgent).FullName);
+
+                    command.CommandText = agent.Hello();
+                    command.ExecuteNonQuery();
+                }
+                finally
+                {
+                    AppDomain.Unload(domain);
+                }
+            }
+        }
+
+        public sealed class StepDomainAgent : MarshalByRefObject
+        {
+            public string Hello()
+            {
+                return "hello";
+            }
+        }
+    }
+}
---
+++ Sources/SqlDatabase/Scripts/AssemblyInternal/DomainDirectory.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace SqlDatabase.Scripts.AssemblyInternal
+{
+    internal sealed class DomainDirectory : IDisposable
+    {
+        private readonly ILogger _logger;
+
+        public DomainDirectory(ILogger logger)
+        {
+            _logger = logger;
+
+            Location = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            Directory.CreateDirectory(Location);
+        }
+
+        public string Location { get; }
+
+        public string SaveFile(byte[] content, string fileName)
+        {
+            var location = Path.Combine(Location, fileName);
+            try
+            {
+                File.WriteAllBytes(location, content);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Fail to copy content of [{0}]: {1}".FormatWith(fileName, ex.Message));
+                Directory.Delete(location);
+                throw;
+            }
+
+            return location;
+        }
+
+        public void Dispose()
+        {
+            if (Directory.Exists(Location))
+            {
+                try
+                {
+                    Directory.Delete(Location, true);
+                }
+                catch (Exception ex)
+                {
+                    _logger.Info("Fail to delete assembly content from {0}: {1}".FormatWith(Location, ex.Message));
+                }
+            }
+        }
+    }
+}
