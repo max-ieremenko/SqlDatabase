4e16df558895e032a5ac139cf53196da1e0e31df
Maksym Ieremenko <maksym.ieremenko@qoniac.com>
2019-01-16 23:08:56 +0100

    fix docker-compose

--- Sources/SqlDatabase.Test/Docker/entrypoint.sh
+++ Sources/SqlDatabase.Test/Docker/entrypoint.sh
@@ -1,5 +1,5 @@
-﻿# wait for SQL Server to come up
-sleep 10
+# wait for SQL Server to come up
+sleep 5s
 
 # create database
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P P@ssw0rd -i ./CreateDatabase.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P P@ssw0rd -l 60 -i ./CreateDatabase.sql
