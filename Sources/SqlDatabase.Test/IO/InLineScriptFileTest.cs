dedcd72bd8bc6595f97153deb0572828d5314aca
Max Ieremenko <max.ieremenko@gmail.com>
2019-04-21 17:19:21 +0200

    -fromSql command line parameter

---
+++ Sources/SqlDatabase.Test/IO/InLineScriptFileTest.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using NUnit.Framework;
+using Shouldly;
+
+namespace SqlDatabase.IO
+{
+    [TestFixture]
+    public class InLineScriptFileTest
+    {
+        [Test]
+        public void OpenRead()
+        {
+            const string Content = "some text";
+
+            var stream = new InLineScriptFile("name", Content).OpenRead();
+            using (stream)
+            {
+                stream.ShouldNotBeNull();
+
+                new StreamReader(stream).ReadToEnd().ShouldBe(Content);
+            }
+        }
+    }
+}
---
+++ Sources/SqlDatabase/IO/InLineScriptFile.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Text;
+
+namespace SqlDatabase.IO
+{
+    internal sealed class InLineScriptFile : IFile
+    {
+        private readonly string _content;
+
+        public InLineScriptFile(string name, string content)
+        {
+            _content = content;
+            Name = name;
+        }
+
+        public string Name { get; }
+
+        public Stream OpenRead()
+        {
+            return new MemoryStream(Encoding.UTF8.GetBytes(_content));
+        }
+    }
+}
