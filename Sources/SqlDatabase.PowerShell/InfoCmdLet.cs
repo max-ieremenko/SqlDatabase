077babf94fead172e1d1dca3dbd2cbf4cfe1581f
Max Ieremenko <max.ieremenko@gmail.com>
2020-03-14 18:26:39 +0100

    Show-SqlDatabaseInfo

---
+++ Sources/SqlDatabase.PowerShell/InfoCmdLet.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Management.Automation;
+using System.Runtime.InteropServices;
+using SqlDatabase.Configuration;
+
+namespace SqlDatabase.PowerShell
+{
+    [Cmdlet(VerbsCommon.Show, "SqlDatabaseInfo")]
+    public sealed class InfoCmdLet : PSCmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            var assembly = GetType().Assembly;
+
+            WriteObject(new
+            {
+                Version = assembly.GetName().Version,
+                RuntimeInformation.FrameworkDescription,
+                RuntimeInformation.OSDescription,
+                Location = Path.GetDirectoryName(assembly.Location),
+                DefaultConfigurationFile = ConfigurationManager.ResolveDefaultConfigurationFile()
+            });
+        }
+    }
+}
