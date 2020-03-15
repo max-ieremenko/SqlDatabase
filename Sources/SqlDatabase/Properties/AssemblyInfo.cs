c4db4211b3e8dc522d6e8442d80823f80eef60f3
Max Ieremenko <max.ieremenko@gmail.com>
2020-03-15 13:21:55 +0100

    update package description

--- Sources/SqlDatabase/Properties/AssemblyInfo.cs
+++ Sources/SqlDatabase/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("SqlDatabase")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SqlDatabase is a tool for SQL Server, allows executing scripts, database migrations and data export.")]
 [assembly: Guid("5ee10884-32f2-443b-b136-66e9a1a3c393")]
 
 [assembly: InternalsVisibleTo("SqlDatabase.PowerShell, PublicKey=002400000480000094000000060200000024000052534131000400000100010055AB0DC1F8A24FB41E7358B65A606EC92141F1ABAFBFF062635AB5FAEB22308CFFBC8B54F3436694F14F6FD6C145D4F16C13A3E739FFCA837902BB78E2D51B890D964CC7384C2CC6B844AE37323F501F29E3EDC2DFADA82C99F5FBB5197ED757D795C2E5408DCB3FBAF9DDDF39E60B137ED0A23603A361EA811E6ADB605DFECC")]
