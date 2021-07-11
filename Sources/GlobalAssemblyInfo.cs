a7ae64ec0df31053d1281d3854ec44e7e69222cf
Max Ieremenko <max.ieremenko@gmail.com>
2021-07-11 15:01:57 +0200

    version 4.0

--- Sources/GlobalAssemblyInfo.cs
+++ Sources/GlobalAssemblyInfo.cs
@@ -9,5 +9,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
